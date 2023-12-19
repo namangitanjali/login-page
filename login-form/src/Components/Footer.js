84a9f81ba7ca1791845f63c73b3b79f3028a2da2
Naman Vyas <116788674+namangitanjali@users.noreply.github.com>
2023-12-20 02:41:43 +0530

    first commit for login page

---
+++ login-form/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-white text-center p-4 mt-3">
+      <h2>Design And Developed By : Naman Vyas</h2>
+      
+    </div>
+  );
+};
+
+export default Footer;
